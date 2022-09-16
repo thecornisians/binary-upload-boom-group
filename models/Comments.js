7f3938456e26e23d36da6bccdcbf92e5f15b6088
ajnage <103336989+ajnage@users.noreply.github.com>
2022-09-16 16:59:03 -0600

    modified commentsschema by adding madeBy(UserID)

--- models/Comments.js
+++ models/Comments.js
@@ -3,9 +3,15 @@ const mongoose = require("mongoose")
 // create comment schema
 const CommentSchema = new mongoose.Schema({
   comment: {type: String, required: true},
-  likes: {type: Number, required: true},
-  // post will be set to what post is commented
-  post: {type: mongoose.Schema.Type.ObjectId, ref: "Post"}
+  // post will be set to what post is commented on
+  postId: {
+    type: mongoose.Schema.Types.ObjectId, 
+    ref: "Post"
+  },
+  madeBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  }
   // timestamps will add a createdAt and updatedAt key value pair
 }, {timestamps: true})
 
