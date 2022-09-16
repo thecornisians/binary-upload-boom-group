2a7cbb65cb71e3ce1acae581740345dee4d07b6b
Patrick Snowden <patrickbsnowden@outlook.com>
2022-09-16 07:59:11 -0700

    added comments

--- controllers/comments.js
+++ controllers/comments.js
@@ -1,14 +1,20 @@
+// require comment model
 const Comments = require("../models/Comments")
 
 module.exports = {
   createComment: async (req,res) => {
     try {
+      // create comment object
       await Comments.create({
+        // comment will come from input in views
         comment: req.body.comment,
+        // likes always start at 0
         likes: 0,
+        // comment belongs to the post with this id
         post: req.params.id,
       })
       console.log("Comment added")
+      // redirect back to post to update changes
       res.redirect(`/post/${req.params.id}`)
     } catch (error) {
       console.log(error)
